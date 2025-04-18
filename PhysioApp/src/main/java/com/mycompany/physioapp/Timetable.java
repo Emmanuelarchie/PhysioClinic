cf2b247682815ca2d1e1a9dd1a46e2df149b0196
Emmanuel Archibong <ea24aan@herts.ac.uk>
2025-04-18 17:20:31 +0100

    Timetable class implementation

--- PhysioApp/src/main/java/com/mycompany/physioapp/Timetable.java
+++ PhysioApp/src/main/java/com/mycompany/physioapp/Timetable.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.physioapp;
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /**
  *
  * @author emman
  */
 public class Timetable {
+    Map<LocalDate, List> appointments;
+    
+    Public Timetable () {
+     this.appointments = newHashMap<>();
+   }
+    
+    public void addAppointment( LocalDate date, Appointment appointment)
+    {
+        if (!appointments.containsKey(date)) {
+            appointments.put(date, new ArrayList<>());
+        }
+        appointments.get(date).add(appointment);
+        
+    }
+    
+    public List<Appointment> getAppointment(LocalDate date){
+        return appointments.getOrDefault(date, new ArrayList<>());
+    }
     
 }
+ 
