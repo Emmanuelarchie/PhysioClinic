8895f3a8adac93fd3b88f226c2079b07421c25d5
Emmanuel Archibong <ea24aan@herts.ac.uk>
2025-04-19 22:03:27 +0100

    Appointment class implementation

--- PhysioApp/src/main/java/com/mycompany/physioapp/Appointments.java
+++ PhysioApp/src/main/java/com/mycompany/physioapp/Appointments.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.physioapp;
-
+import java.time.LocalTime;
 /**
  *
  * @author emman
  */
-public class Appointments {
+public class Appointment {
     
+     String treatmentName; 
+     LocalTime time; 
+     Patient patient; 
+     String status; // booked, cancelled, attended
+     
+     public Appointment(String treatmentName, LocalTime time) {
+    this.treatmentName = treatmentName;
+    this.time = time;
+    this.status = "available";
+     }
+     
+     public void book(Patient patient) {
+    if (this.status.equals("available")) {
+        this.patient = patient;
+        this.status = "booked";
+    }
+     
+     
+     }
+     
+     
+     public void cancel() {
+    if (this.status.equals("booked")) {
+        this.patient = null;
+        this.status = "cancelled";
+    }
 }
+
+public void attend() {
+    if (this.status.equals("booked")) {
+        this.status = "attended";
+    }
+}
+     
+}
+
+
